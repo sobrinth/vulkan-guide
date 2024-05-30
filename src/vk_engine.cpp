772a1bfd8faf0ae5275ed118c18766dc0b8316d7
Dominik Baumann <hey@sobrinth.com>
2024-05-30 22:30:28 +0200

    Vulkan initialization code

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -7,12 +7,18 @@
 #include <vk_initializers.h>
 #include <vk_types.h>
 
+// bootstrap library
+#include "VkBootstrap.h"
+
 #include <chrono>
 #include <thread>
 
 VulkanEngine* loadedEngine = nullptr;
 
 VulkanEngine& VulkanEngine::Get() { return *loadedEngine; }
+
+constexpr bool bUseValidationLayers = true;
+
 void VulkanEngine::init()
 {
     // only one engine initialization is allowed with the application.
@@ -32,13 +38,29 @@ void VulkanEngine::init()
         _windowExtent.height,
         window_flags);
 
+    init_vulkan();
+
+    init_swapchain();
+
+    init_commands();
+
+    init_sync_structures();
+
     // everything went fine
     _isInitialized = true;
 }
 
 void VulkanEngine::cleanup()
 {
-    if (_isInitialized) {
+    if (_isInitialized)
+    {
+        destroy_swapchain();
+
+        vkDestroySurfaceKHR(_instance, _surface, nullptr);
+        vkDestroyDevice(_device, nullptr);
+
+        vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
+        vkDestroyInstance(_instance, nullptr);
 
         SDL_DestroyWindow(_window);
     }
@@ -58,25 +80,31 @@ void VulkanEngine::run()
     bool bQuit = false;
 
     // main loop
-    while (!bQuit) {
+    while (!bQuit)
+    {
         // Handle events on queue
-        while (SDL_PollEvent(&e) != 0) {
+        while (SDL_PollEvent(&e) != 0)
+        {
             // close the window when user alt-f4s or clicks the X button
             if (e.type == SDL_QUIT)
                 bQuit = true;
 
-            if (e.type == SDL_WINDOWEVENT) {
-                if (e.window.event == SDL_WINDOWEVENT_MINIMIZED) {
+            if (e.type == SDL_WINDOWEVENT)
+            {
+                if (e.window.event == SDL_WINDOWEVENT_MINIMIZED)
+                {
                     stop_rendering = true;
                 }
-                if (e.window.event == SDL_WINDOWEVENT_RESTORED) {
+                if (e.window.event == SDL_WINDOWEVENT_RESTORED)
+                {
                     stop_rendering = false;
                 }
             }
         }
 
         // do not draw if we are minimized
-        if (stop_rendering) {
+        if (stop_rendering)
+        {
             // throttle the speed to avoid the endless spinning
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
             continue;
@@ -85,3 +113,109 @@ void VulkanEngine::run()
         draw();
     }
 }
+
+void VulkanEngine::init_vulkan()
+{
+    vkb::InstanceBuilder builder;
+
+    // make vulkan instance, with basic debug features
+    auto inst_ret = builder.set_app_name("Another stupid vulkan things")
+                           .request_validation_layers(bUseValidationLayers)
+                           .use_default_debug_messenger()
+                           .require_api_version(1, 3, 0)
+                           .build();
+
+    // TODO: Error checking on Result<T> types!
+    vkb::Instance vkb_instance = inst_ret.value();
+
+    // grab the instance
+    _instance = vkb_instance.instance;
+    _debug_messenger = vkb_instance.debug_messenger;
+
+
+    SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
+
+    // vulkan 1.3 features
+    VkPhysicalDeviceVulkan13Features features13{};
+    features13.dynamicRendering = true;
+    features13.synchronization2 = true;
+
+    // vulkan 1.2 features
+    VkPhysicalDeviceVulkan12Features features12{};
+    features12.bufferDeviceAddress = true;
+    features12.descriptorIndexing = true;
+
+    // use vkbootstrap to select a gpu because i am lazy
+    // GPU must support all requested features (obviously)
+    vkb::PhysicalDeviceSelector selector{vkb_instance};
+    vkb::PhysicalDevice physicalDevice = selector
+                                         .set_minimum_version(1, 3)
+                                         .set_required_features_13(features13)
+                                         .set_required_features_12(features12)
+                                         .set_surface(_surface)
+                                         .select()
+                                         .value();
+
+    // create the final vulkan device
+    vkb::DeviceBuilder deviceBuilder{physicalDevice};
+
+    vkb::Device vkbDevice = deviceBuilder.build().value();
+
+    // Get the device handle used in the rest of the vulkan application
+    _device = vkbDevice.device;
+    _chosenGPU = physicalDevice.physical_device;
+}
+
+void VulkanEngine::init_swapchain()
+{
+    create_swapchain(_windowExtent.width, _windowExtent.height);
+    // not implemented yet :3
+}
+
+void VulkanEngine::init_commands()
+{
+    // not implemented yet :3
+}
+
+void VulkanEngine::init_sync_structures()
+{
+    // not implemented yet :3
+}
+
+
+void VulkanEngine::create_swapchain(uint32_t width, uint32_t height)
+{
+    vkb::SwapchainBuilder swapchainBuilder{_chosenGPU, _device, _surface};
+
+    _swapchainImageFormat = VK_FORMAT_B8G8R8_UNORM;
+
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+                                  //.use_default_format_selection()
+                                  .set_desired_format(VkSurfaceFormatKHR{
+                                      .format = _swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR
+                                  })
+                                  // use vsync present mode
+                                  .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+                                  .set_desired_extent(width, height)
+                                  .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+                                  .build()
+                                  .value();
+
+    _swapchainExtent = vkbSwapchain.extent;
+    // store swapchain and its related images
+    _swapchain = vkbSwapchain.swapchain;
+    _swapchainImages = vkbSwapchain.get_images().value();
+    _swapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+void VulkanEngine::destroy_swapchain()
+{
+    vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+    // destory individual swapchain resources
+    for (int i = 0; i < _swapchainImageViews.size(); i++)
+    {
+        vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
+    }
+}
+
